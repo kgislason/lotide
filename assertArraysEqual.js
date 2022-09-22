b501a9850c52346e7f10ff0ab3fecf1e5e169a16
Kristy Gislason <kristygislason@gmail.com>
2022-09-22 09:18:29 -0700

    Implement assertArraysEqual function that prints a message to the console if eqArrays returns true or false

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  const passMessage = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  const failMessage = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+
+  console.log((actual === expected) ? passMessage : failMessage);
+};
+
+const eqArrays = (arrayOne, arrayTwo) => {
+  if (!arrayOne.length === arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (arrayOne, arrayTwo) => {
+  assertEqual(eqArrays(arrayOne, arrayTwo), true);
+}
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([3, 2, 1], [1, 2, 3]); // => should PASS
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [1, 2]); // => should PASS
