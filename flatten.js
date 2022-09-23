1ceb77898a6138739cd64116240789fcae8722d7
Kristy Gislason <kristygislason@gmail.com>
2022-09-22 21:48:43 -0700

    Rewrote flatten to not use .flat(Infinity) - which seemed too easy

--- flatten.js
+++ flatten.js
@@ -22,14 +22,25 @@ const assertArraysEqual = (arrayOne, arrayTwo) => {
 };
 
 const flatten = (array) => {
-  let newArray = array.flat(Infinity);
+  // let newArray = array.flat(Infinity);
+  // return newArray;
+  let newArray = [];
+
+  for (let value of array) {
+    if (Array.isArray(value)) {
+      for (let item of value) {
+        newArray.push(item);
+      }
+    } else if (!Array.isArray(value)) {
+      newArray.push(value);
+    }
+  }
+
   return newArray;
 };
 
+// Test cases
 console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
-
 assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
-
-
-console.log(flatten([1, 2, [3, [[4]]], [5], [6]]));
+console.log(flatten([1, 2, [3, 4], [5], [6]]));
 assertArraysEqual(flatten([1, 2, [3, 4], [5], [6]]), [1, 2, 3, 4, 5, 6]);
