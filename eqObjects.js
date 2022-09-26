b84eb30f1bc0caecf3a1e84f7a337478c64d837a
Kristy Gislason <kristygislason@gmail.com>
2022-09-26 10:59:47 -0700

    Ram through eslint and tests again to ensure everything is working as expected

--- eqObjects.js
+++ eqObjects.js
@@ -44,44 +44,44 @@ const eqObjects = (object1, object2) => {
   return true;
 };
 
+// Tests with Primitive Data Types
+const ab = { a: "1", b: {} };
+const ba = { b: "2", a: "1" };
+console.log(eqObjects(ab, ba)); // => true
 
-// Tests
-// const ab = { a: "1", b: {} };
-// const ba = { b: "2", a: "1" };
-// console.log(eqObjects(ab, ba)); // => true
+const abc = { a: "1", b: "2", c: "3" };
+console.log(eqObjects(ab, abc)); // => false
 
-// const abc = { a: "1", b: "2", c: "3" };
-// console.log(eqObjects(ab, abc)); // => false
+const human = {
+  name: "Jane Smith",
+  age: 50,
+  city: "Victoria"
+};
 
-// const human = {
-//   name: "Jane Smith",
-//   age: 50,
-//   city: "Victoria"
-// };
+const animal = {
+  name: "Nibbles",
+  age: 2,
+  city: "Victoria"
+};
 
-// const animal = {
-//   name: "Nibbles",
-//   age: 2,
-//   city: "Victoria"
-// };
+console.log(assertEqual(human["name"], animal["name"])); // => false
+console.log(eqObjects(human, animal)); // => false
 
-// console.log(assertEqual(human["name"], animal["name"])); // => false
-// console.log(eqObjects(human, animal)); // => false
+const example1 = {
+  sample: "a",
+  test: true,
+  result: "pass",
+};
 
-// const example1 = {
-//   sample: "a",
-//   test: true,
-//   result: "pass",
-// };
+const example2 = {
+  test: true,
+  result: "pass",
+  sample: "a",
+};
 
-// const example2 = {
-//   test: true,
-//   result: "pass",
-//   sample: "a",
-// };
+console.log(eqObjects(example1, example2)); // => true
 
-// console.log(eqObjects(example1, example2)); // => true
-
+// Test Cases with Arrays
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 console.log(eqObjects(cd, dc)); // => true
@@ -89,5 +89,5 @@ console.log(eqObjects(cd, dc)); // => true
 const cd2 = { c: "1", d: ["2", 3, 4] };
 console.log(eqObjects(cd, cd2)); // => false
 console.log(eqArrays(cd["d"], cd2["d"])); // => false
-console.log(assertEqual(cd["d"].join(), dc["d"].join() ));
-console.log(assertEqual(cd["d"].join(), cd2["d"].join() ));
+console.log(assertEqual(cd["d"][0], dc["d"][0]));
+console.log(assertEqual(cd["d"].length, cd2["d"].length));
