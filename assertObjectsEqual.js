c63084f57976e262e0c41c5c034c8a9ce0c4fc6d
Kristy Gislason <kristygislason@gmail.com>
2022-09-26 11:22:18 -0700

    Add in util library inspect function so objects show up in the message printed

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -24,8 +24,9 @@ const eqObjects = (object1, object2) => {
 };
 
 const assertObjectsEqual = (actual, expected) => {
-  const passMessage = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  const failMessage = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  const inspect = require('util').inspect;
+  const passMessage = `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`;
+  const failMessage = `🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`;
 
   console.log((eqObjects(actual, expected) === true) ? passMessage : failMessage);
 };
@@ -38,4 +39,10 @@ const object2 = {
   name: "Kristy",
 }
 
+const object3 = {
+  name: "Kristy",
+  city: "Victoria",
+}
+
 assertObjectsEqual(object1, object2); // => true
+assertObjectsEqual(object1, object3); // => false
