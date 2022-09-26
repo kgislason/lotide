ff3d7d03325c02fdfc54f9fd5d8fb48f12e2f718
Kristy Gislason <kristygislason@gmail.com>
2022-09-25 21:07:22 -0700

    Refactored as misunderstood instructions - the value is an array and not a string, wrote tests for comparing arrays

--- letterPositions.js
+++ letterPositions.js
@@ -25,15 +25,40 @@ const letterPositions = function(sentence) {
   const results = {};
   const sentenceArray = sentence.split('');
 
-  for (let letter in sentenceArray) {
-    if (sentenceArray[letter] !== ' ') {
-      results[letter] = sentenceArray[letter];
+  for (let index in sentenceArray) {
+    if (sentenceArray[index] !== ' ') {
+      if (results[sentenceArray[index]]) {
+        results[sentenceArray[index]].push(Number(index));
+      } else {
+        results[sentenceArray[index]] = [Number(index)];
+      }
     }
   }
   // logic to update results here
   return results;
 };
 
-const exampleString = ("lighthouse in the house");
+const exampleString = "lighthouse in the house";
+const exampleString2 = "only";
 
 console.log(letterPositions(exampleString));
+console.log(letterPositions(exampleString)["l"]);
+console.log(letterPositions(exampleString2)["l"]);
+
+// Test
+assertArraysEqual(letterPositions(exampleString)["l"], [0]);
+assertArraysEqual(letterPositions(exampleString2)["l"], [2]);
+
+// Expected Outcome
+// {
+//   l: [0],
+//   i: [1, 11],
+//   g: [2],
+//   h: [3, 5, 15, 18],
+//   t: [4, 14],
+//   o: [6, 19],
+//   u: [7, 20],
+//   s: [8, 21],
+//   e: [9, 16, 22],
+//   n: [12]
+// }
