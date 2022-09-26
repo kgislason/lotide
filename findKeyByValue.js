cf327d32d34d2450a472c28597c10993609fe5b2
Kristy Gislason <kristygislason@gmail.com>
2022-09-25 21:17:15 -0700

    Wrote the function and seems to work as expected. Ran through eslint and test cases pass

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  const passMessage = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  const failMessage = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+
+  console.log((actual === expected) ? passMessage : failMessage);
+};
+
+
+// takes in an object and a value.
+const findKeyByValue = (object, value) => {
+  // scan the object
+  for (let key in object) {
+    // return the first key which contains the given value.
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  // If no key with that given value is found,
+  // then it should return undefined.
+  return;
+};
+
+// Tests
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
