d94fa9c464fe60bbb461d75b6852f059a7436bd9
Kristy Gislason <kristygislason@gmail.com>
2022-09-28 11:42:51 -0700

    Eslint and fixed messages for missing eqArrays

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,8 +1,13 @@
-const assertEqual = function(actual, expected) {
-  const passMessage = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
-  const failMessage = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
-
-  console.log((actual === expected) ? passMessage : failMessage);
+const eqArrays = (arrayOne, arrayTwo) => {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const eqObjects = (object1, object2) => {
