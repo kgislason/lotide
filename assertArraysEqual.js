6e012f295fb5a278adc6a02259b0f0d52c0b0dbe
Kristy Gislason <kristygislason@gmail.com>
2022-09-28 11:44:05 -0700

    eslinted and fixed message by adding in assertEqual call

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -23,7 +23,7 @@ const assertArraysEqual = (actual, expected) => {
   console.log((eqArrays(actual, expected) === true) ? passMessage : failMessage);
 };
 
-
+assertEqual([1, 2, 3].length, [1, 2, 3].length); // => should PASS
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
 assertArraysEqual([3, 2, 1], [1, 2, 3]); // => should PASS
