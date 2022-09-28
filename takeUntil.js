d970a8072c1e36502c8b5ebb58a8fbb95bf03f48
Kristy Gislason <kristygislason@gmail.com>
2022-09-28 11:39:56 -0700

    Write function takeUntil and test cases

---
+++ takeUntil.js
@@ -0,0 +1,52 @@
+/**
+ * Function: tankeUntil
+ *
+ * Returns a slice of the array with elements taken from the beginning
+ */
+
+const eqArrays = (arrayOne, arrayTwo) => {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  const passMessage = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  const failMessage = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  console.log((eqArrays(actual, expected) === true) ? passMessage : failMessage);
+};
+
+// take in two parameters:
+// 1. array to work with
+// 2. callback or predicate
+//   a. callback should only be provided one value: The item in the array
+const takeUntil = function(array, callback) {
+  const indexEnd = array.findIndex(callback);
+  return array.slice(0, indexEnd);
+};
+
+// Test Data
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+
+// Expected Output
+// [ 1, 2, 5, 7, 2 ]
+// --
+// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+
+// Example Function Calls
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+console.log('---');
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+// Tests
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
