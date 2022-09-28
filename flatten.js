4c6579ed830d7fde5a010b6b260d815c23858a2e
Kristy Gislason <kristygislason@gmail.com>
2022-09-28 11:53:25 -0700

    Eslint and other improvements to flatten.js - change let to const, check if array is provided as argument

--- flatten.js
+++ flatten.js
@@ -23,26 +23,38 @@ const assertArraysEqual = (actual, expected) => {
   console.log((eqArrays(actual, expected) === true) ? passMessage : failMessage);
 };
 
-const flatten = (array) => {
+const flatten = function(myArray) {
   // let newArray = array.flat(Infinity);
   // return newArray;
-  let newArray = [];
+  const newArray = [];
 
-  for (let value of array) {
-    if (Array.isArray(value)) {
-      for (let item of value) {
-        newArray.push(item);
+  // Check if argument is actually an array
+  if(!Array.isArray(myArray)) {
+    return;
+  }
+
+  for (const x of myArray) {
+    if (Array.isArray(x)) {
+      for (const y of x) {
+        newArray.push(y);
       }
-    } else if (!Array.isArray(value)) {
-      newArray.push(value);
+    } else if (!Array.isArray(x)) {
+      newArray.push(x);
     }
   }
 
   return newArray;
 };
 
-// Test cases
-console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
-console.log(flatten([1, 2, [3, 4], [5], [6]]));
-assertArraysEqual(flatten([1, 2, [3, 4], [5], [6]]), [1, 2, 3, 4, 5, 6]);
+// Test Data
+const array1 = [1, 2, [3, 4], 5, [6]];
+const array2 = [1, 2, [3, 4], [5], [6]];
+const arrayFlat = [1, 2, 3, 4, 5, 6];
+
+// Test Cases
+console.log(flatten(array1)); // => [1, 2, 3, 4, 5, 6]
+assertEqual(flatten(array1.length, arrayFlat)); // =>  Should pass
+assertArraysEqual(flatten(array1), arrayFlat);
+console.log("----");
+console.log(flatten(array2));
+assertArraysEqual(flatten(array2), arrayFlat);
