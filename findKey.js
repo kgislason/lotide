620d77ba10db7ee5867abf58c103f04836641014
Kristy Gislason <kristygislason@gmail.com>
2022-09-28 13:28:12 -0700

    Wrote 3 tests and eslinted

--- findKey.js
+++ findKey.js
@@ -1,9 +1,9 @@
 /**
  * Function: findKey
- * 
+ *
  */
 
-assertEqual = function(actual, expected) {
+const assertEqual = function(actual, expected) {
   const passMessage = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
   const failMessage = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
 
@@ -25,14 +25,20 @@ const findKey = function(object, callback) {
   return;
 };
 
-// Test 
-const test1 = findKey({
+// Test
+const testObject = {
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+};
+
+const test1 = findKey(testObject, x => x.stars === 2); // => "noma"
+const test2 = findKey(testObject, x => x.stars < 3); // => "Blue Hill"
+const test3 = findKey(testObject, x => x.stars === 4); // => undefined
 
 assertEqual(test1, "noma");
+assertEqual(test2, "Blue Hill");
+assertEqual(test3, undefined);
