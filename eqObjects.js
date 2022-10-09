1c038886819614af08372a23fd55f12e653f3977
Kristy Gislason <kristygislason@gmail.com>
2022-10-09 14:57:36 -0700

    Refactor eqObjects to use recursive function

--- eqObjects.js
+++ eqObjects.js
@@ -1,7 +1,6 @@
 const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
 
-// Check if 2 objects are the same: same number of keys, same key values, same  arrays foudn within key values
 const eqObjects = function(object1, object2) {
   // create two arrays of the object keys
   let objectKey1 = Object.keys(object1);
@@ -20,58 +19,15 @@ const eqObjects = function(object1, object2) {
       if (! eqArrays(object1[key], object2[key])) {
         return false;
       }
+    } else if (typeof object1[key] === 'object') {
+      // Check if is nested object that is not array
+      return eqObjects(object1[key], object2[key]);
     } else if (object1[key] !== object2[key]) {
       // compare primitive data types
       return false;
     }
   }
   return true;
-};
+}
 
-// Tests with Primitive Data Types
-const ab = { a: "1", b: {} };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
-
-const human = {
-  name: "Jane Smith",
-  age: 50,
-  city: "Victoria"
-};
-
-const animal = {
-  name: "Nibbles",
-  age: 2,
-  city: "Victoria"
-};
-
-console.log(assertEqual(human["name"], animal["name"])); // => false
-console.log(eqObjects(human, animal)); // => false
-
-const example1 = {
-  sample: "a",
-  test: true,
-  result: "pass",
-};
-
-const example2 = {
-  test: true,
-  result: "pass",
-  sample: "a",
-};
-
-console.log(eqObjects(example1, example2)); // => true
-
-// Test Cases with Arrays
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
-console.log(eqArrays(cd["d"], cd2["d"])); // => false
-console.log(assertEqual(cd["d"][0], dc["d"][0]));
-console.log(assertEqual(cd["d"].length, cd2["d"].length));
+module.exports = eqObjects;
