fa14ff66cdd5d3e6f05f124e8d572ec1125775cd
Kristy Gislason <kristygislason@gmail.com>
2022-10-09 14:59:41 -0700

    move test code to new file for eqArrays

---
+++ test/eqArrays.test.js
@@ -0,0 +1,16 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+// Test Cases
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+// Use assertEqual to write test cases for various scenarios.
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([3, 2, 1], [1, 2, 3]), false); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false); // => should PASS
