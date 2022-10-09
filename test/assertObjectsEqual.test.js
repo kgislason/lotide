349b9d53637ad7b7edcbb8aec456046a72e14838
Kristy Gislason <kristygislason@gmail.com>
2022-10-09 14:59:15 -0700

    Move assertObjectsEqual test code to new file

---
+++ test/assertObjectsEqual.test.js
@@ -0,0 +1,20 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+const eqObjects = require('../eqObjects');
+const assertObjectsEqual = require('../assertObjectsEqual');
+
+const object1 = {
+  name: "Kristy",
+};
+
+const object2 = {
+  name: "Kristy",
+};
+
+const object3 = {
+  name: "Kristy",
+  city: "Victoria",
+};
+
+assertObjectsEqual(object1, object2); // => true
+assertObjectsEqual(object1, object3); // => false
