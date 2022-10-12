a2be4d6c5b6a329ee3a6ea0a4e050b755f254b57
Kristy Gislason <kristygislason@gmail.com>
2022-10-11 20:34:52 -0700

    Refactor tests so they use mocha and chai for map()

--- map.js
+++ map.js
@@ -1,5 +1,18 @@
+/**
+ * map.js
+ *
+ * take in two arguments:
+ * - An array to map
+ * - A callback function
+ *
+ * Description: The map function will return a new array based on the results of the callback function.
+ */
 const map = function(array, callback) {
   const result = [];
+
+  if (!Array.isArray(array)) return;
+
+  if (!callback) return;
 
   for (let item of array) {
     result.push(callback(item));
--- test/map.test.js
+++ test/map.test.js
@@ -1,7 +1,7 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const map = require('../map');
 
-
+// Define arrays to work with
 const words = ["ground", "control", "to", "major", "tom"];
 const animals = [
   {
@@ -22,29 +22,48 @@ const animals = [
   }
 ];
 
-// Test
-const wordsFirstLetter = map(words, word => word[0]);
-console.log(wordsFirstLetter);
+describe("#map", () => {
 
-const animalNames = map(animals, x => x.name);
-console.log(animalNames);
+  it('should return first letter of all words in array words', () => {
+    const actual = map(words, word => word[0]);
+    const expected = [ 'g', 'c', 't', 'm', 't' ];
+    assert.deepEqual(actual, expected);
+  });
 
-const animalTypes = map(animals, x => x.type);
-console.log(animalTypes);
+  it('should return animals names from object animals, ["Goldie", "Lizzie", "Nibbles", "Sasha"]', () => {
+    // const animalNames = map(animals, x => x.name);
+    const actual = map(animals, x => x.name);
+    const expected = [ 'Goldie', 'Lizzie', 'Nibbles', 'Sasha' ];
+    assert.deepEqual(actual, expected);
+  });
 
-const wordsLength = map(words, x => `Word Length: ${x.length}`);
-console.log(wordsLength);
+  it('should return word lengths - Word Length: 6... in an array', () => {
+    const actual = map(words, x => `Word Length: ${x.length}`);
+    const expected = ["Word Length: 6", "Word Length: 7", "Word Length: 2", "Word Length: 5", "Word Length: 3"
+    ];
+    assert.deepEqual(actual, expected);
+  });
 
-console.log(assertArraysEqual(wordsFirstLetter, [ 'g', 'c', 't', 'm', 't' ]));
+  it('should return animal types - [ "dog", "fish", "Hamster", "dog"]', () => {
+    const actual = map(animals, x => x.type);
+    const expected = [ 'dog', 'fish', 'Hamster', 'dog'];
+    assert.deepEqual(actual, expected);
+  });
 
-console.log(assertArraysEqual(animalNames, [ 'Goldie', 'Lizzie', 'Nibbles', 'Sasha' ]));
+  it('should return undefined if the map() function has no arguments map()', () => {
+    const actual = map();
+    const expected = undefined;
+    assert.strictEqual(actual, expected);
+  });
 
-console.log(assertArraysEqual(animalTypes, [ 'dog', 'fish', 'Hamster', 'dog' ]));
-
-console.log(assertArraysEqual(wordsLength, [
-  'Word Length: 6',
-  'Word Length: 7',
-  'Word Length: 2',
-  'Word Length: 5',
-  'Word Length: 3'
-]));
+  it('should return undefined if the map() function has no function argument - map([1, 2, 3])', () => {
+    const actual = map([1, 2, 3]);
+    const expected = undefined;
+    assert.strictEqual(actual, expected);
+  });
+  it('should return undefined if the argument passed is not an array', () => {
+    const actual = map('string', x => x < 0);
+    const expected = undefined;
+    assert.strictEqual(actual, expected);
+  });
+});
