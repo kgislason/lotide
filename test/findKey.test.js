ffb3120ecaefe8551424c17af064ef471c51dd55
Kristy Gislason <kristygislason@gmail.com>
2022-10-12 08:57:28 -0700

    Refactor findKey() to use mocha and chai

--- test/findKey.test.js
+++ test/findKey.test.js
@@ -1,20 +1,38 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const findKey = require('../findKey');
 
-// Test
-const testObject = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-};
+describe("#findKey", () => {
+  // Test
+  const testObject = {
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  };
 
-const test1 = findKey(testObject, x => x.stars === 2); // => "noma"
-const test2 = findKey(testObject, x => x.stars < 3); // => "Blue Hill"
-const test3 = findKey(testObject, x => x.stars === 4); // => undefined
+  it('should return key drama for value The Wire', () => {
+    const actual = findKey(testObject, x => x.stars === 2);
+    const expected = "noma";
+    assert.strictEqual(actual, expected);
+  });
 
-assertEqual(test1, "noma");
-assertEqual(test2, "Blue Hill");
-assertEqual(test3, undefined);
+  it('should return undefined for a value that does not exist', () => {
+    const actual = findKey(testObject, x => x.stars < 3);
+    const expected = "Blue Hill";
+    assert.strictEqual(actual, expected);
+  });
+  
+  it('should return undefined if no arguments are passed to function', () => {
+    const actual = findKey(testObject, x => x.stars === 4);
+    const expected = undefined;
+    assert.strictEqual(actual, expected);
+  });
+
+  it('should return undefined if no arguments are passed to function', () => {
+    const actual = findKey();
+    const expected = undefined;
+    assert.strictEqual(actual, expected);
+  });
+});
