9c954a19c39fce59445a7265c5d9426f7aece660
Kristy Gislason <kristygislason@gmail.com>
2022-10-11 17:18:37 -0700

    Add comment at top of file

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,10 @@
-// FUNCTION IMPLEMENTATION - Assert Equal
+/**
+ *
+ * assertEqual.js
+ *
+ * A function that takes in 2 values and returns true if they are equal or false otherwise
+ *
+ */
 const assertEqual = function(actual, expected) {
   const passMessage = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
   const failMessage = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
